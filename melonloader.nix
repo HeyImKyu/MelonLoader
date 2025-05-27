3057e907e85a59d10637b567e1031c72244ad4ba
Kyu <heyimkyu@mailbox.org>
2025-05-27 17:52:28 +0200

    pin up

--- melonloader.nix
+++ melonloader.nix
@@ -15,8 +15,8 @@ buildDotnetModule rec {
   src = fetchFromGitHub {
     owner = "HeyImKyu";
     repo = "MelonLoader";
-    rev = "6c320b45d43e7a78a9c89be27fb705dc872b531d";
-    sha256 = "sha256-Y6UUWjpFEoQmQ9gmM93mIqQNhNb5IO+JhipFGTj4E6A=";
+    rev = "aa5794baa1103bc98f13e2b70250750ec2524200";
+    sha256 = "sha256-TFVsHrw4MlHH1qqGI6GlZE2ZOnunvzZrSTPKIZUWi6E=";
   };
 
   projectFile = "MelonLoader.Bootstrap/MelonLoader.Bootstrap.csproj";

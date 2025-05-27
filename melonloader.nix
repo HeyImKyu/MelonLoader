23f3ccf834e6d68c8b26f5320a19f280a0efea66
Kyu <heyimkyu@mailbox.org>
2025-05-27 12:58:55 +0200

    trying to use dotnet 6

--- melonloader.nix
+++ melonloader.nix
@@ -1,16 +1,20 @@
 { buildDotnetModule
 , fetchFromGitHub
 , lib
+, dotnetCorePackages
 }:
 
 buildDotnetModule rec {
   pname = "melonloader";
   version = "0.0.1";
 
+  dotnet-sdk = dotnetCorePackages.sdk_6_0;
+  dotnet-runtime = dotnetCorePackages.runtime_6_0;
+
   src = fetchFromGitHub {
     owner = "HeyImKyu";
     repo = "MelonLoader";
-    rev = "081d216bc97c8913409faab16df22b0e13b5a82a";
+    rev = "01c70cbb7c90b88fb1160e6dd1d0581f6c1417ea";
     sha256 = "sha256-9yIwnYAoRJyFeRfOjQ+6di9Z/iWHaPjMXPa6olj4r6s=";
   };
 
